4331efc428544c9277a1900ab81ec8fdb93bdb0c
Wilfredo Diaz <wdiazdev@gmail.com>
2023-04-13 15:18:14 -0400

    refactory

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Form, Col, Row, Input, Skeleton } from 'antd'
 import { Colorpicker } from 'antd-colorpicker'
 import './app.css';
